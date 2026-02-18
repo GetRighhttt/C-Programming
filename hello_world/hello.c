53e3354d05d8939523e9bf866db629b84c89c755
Stefan Bayne <stefanbayne@gmail.com>
2026-02-18 08:29:10 -0600

    Updated code comments for hello.c

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h> // preprocessor directive
 
+// main inits the start of the program
 int main() {
  
-	printf("Hello World!\n"); 
+	printf("Hello World!\n"); // /n is newline character 
 	printf("This is my first 'C' program!\n");
 	printf("My name is Stefan and I love Coding!\n");
 	printf("I used to know C++ but I forgot it...so I said instead\n");
