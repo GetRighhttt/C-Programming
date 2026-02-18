26edd4f070e04df79b4f16b5ba007394b5d48f79
Stefan Bayne <stefanbayne@gmail.com>
2026-02-18 05:56:17 -0600

    First hello world commit

---
+++ hello_world/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // preprocessor directive
+
+int main() {
+ 
+	printf("Hello World!\n"); 
+	printf("This is my first 'C' program!\n");
+	printf("My name is Stefan and I love Coding!\n");
+	printf("I used to know C++ but I forgot it...so I said instead\n");
+	printf("Why not learn C and systems programming?\n");
+
+
+	return 0; // signals everything is ok
+}
